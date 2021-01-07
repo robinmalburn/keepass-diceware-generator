90a79a6ccb630e4422d2fb819d47bbb588bb8c73
Robin Malburn <robin.malburn@googlemail.com>
2021-01-07 21:38:03 +0000

    Fixes various style cop issues with Models namespace.

--- Models/Config.cs
+++ Models/Config.cs
@@ -7,71 +7,119 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Xml;
-using System.Xml.Serialization;
-using System.Text;
-using DicewareGenerator.Repositories;
-
 namespace DicewareGenerator.Models
 {
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using DicewareGenerator.Repositories;
+    
     /// <summary>
     /// Config Model.
     /// </summary>
     public class Config
     {
-        [XmlAttribute("space", Namespace = "http://www.w3.org/XML/1998/namespace")]  
-        public string Space="preserve";
-     
-        public decimal NumberOfWords = 6;
-        
-        public bool StudlyCaps = false;
-        
-        public DicewareFileType Wordlist = DicewareFileType.Short;
-        
-        public string Separator = " ";
-        
-        public bool SpecialChars = false;
-        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Config"/> class.
+        /// </summary>
         public Config()
         {
-            
+            this.NumberOfWords = 6;
+            this.StudlyCaps = false;
+            this.Wordlist = DicewareFileType.Short;
+            this.Separator = " ";
+            this.SpecialChars = false;
         }
+        
+        /// <summary>
+        /// Gets the <code>space</code> attribute setting for XML serialization.
+        /// </summary>
+        [XmlAttribute("space", Namespace = "http://www.w3.org/XML/1998/namespace")]  
+        public string Space
+        {
+            get
+            {
+                return "preserve";
+            }
+        }
+        
+        /// <summary>
+        /// Gets or sets the number of words to be generated.
+        /// </summary>
+        public decimal NumberOfWords { get; set; }
+        
+        /// <summary>
+        /// Gets or sets a value indicating whether to upper case the first letter of each word.
+        /// </summary>
+        public bool StudlyCaps { get; set; }
+        
+        /// <summary>
+        /// Gets or sets a value indicating which <see cref="DicewareFileType"/> to use.
+        /// </summary>
+        public DicewareFileType Wordlist { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the separator string to use.
+        /// </summary>
+        public string Separator { get; set; }
+        
+        /// <summary>
+        /// Gets or sets a value indicating whether to mix in special characters.
+        /// </summary>
+        public bool SpecialChars { get; set; }
                 
-        static public string serialize(Config config)
+        /// <summary>
+        /// Serialize the given <see cref="Config"/>
+        /// </summary>
+        /// <param name="config">The Config instance.</param>
+        /// <returns>A string of serialized XML.</returns>
+        public static string Serialize(Config config)
         {   
-            XmlSerializer serializer = new XmlSerializer(config.GetType());
+            var serializer = new XmlSerializer(config.GetType());
             
-            using (MemoryStream stream = new MemoryStream()) {
-                
-                using (XmlWriter writer = XmlWriter.Create(stream, new XmlWriterSettings() {
+            using (var stream = new MemoryStream())
+            {
+                var settings = new XmlWriterSettings() 
+                {
                     Encoding = new UTF8Encoding(false),
                     Indent = false,
-                    NewLineOnAttributes = false}
-                   )
-                ) {
+                    NewLineOnAttributes = false
+                };
+                
+                using (XmlWriter writer = XmlWriter.Create(stream, settings))
+                {
                     serializer.Serialize(writer, config);
                     return Encoding.UTF8.GetString(stream.ToArray());
                 }
             }
         }
         
-        static public Config deserialize(string raw)
+        /// <summary>
+        /// Deserialize an XML string into a <see cref="Config"/> model
+        /// </summary>
+        /// <param name="raw">An XML string.</param>
+        /// <returns><see cref="Config"/> Model</returns>
+        public static Config Deserialize(string raw)
         {
-            Config config = new Config();
+            var config = new Config();
             
-            if (string.IsNullOrEmpty(raw)) {
+            if (string.IsNullOrEmpty(raw))
+            {
                 return config;
             }
             
-            XmlSerializer serializer = new XmlSerializer(config.GetType());
+            var serializer = new XmlSerializer(config.GetType());
             
-            using (StringReader reader = new StringReader(raw)) {
-                try {
-                return (Config)serializer.Deserialize(reader);
-                } catch (InvalidOperationException) {
+            using (var reader = new StringReader(raw))
+            {
+                try 
+                {
+                    return (Config)serializer.Deserialize(reader);
+                } 
+                catch (InvalidOperationException)
+                {
                     return config;
                 }
             }
